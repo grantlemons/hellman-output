0d8512a29cbeda1a7086447623ed7f4bd2aa95a0
Grant Lemons <grantlemons@aol.com>
2025-02-19 09:39:17 -0700

    add doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use std::{fmt::Display, str::FromStr, string::ParseError};
 
+/// Helper for the following requirements for output:
+///
+/// - Any stdout output line with a required result must have OUTPUT as the first word (token).
+/// - All numerical values required by an assignment must have whitespace (which includes end of line) around them.
+/// - Required textual output should be surrounded by single colons (:).
+/// - The order for results is assignment specific and important, they must emanate from the application in the order dictated by the assignment.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct HellmanOutput(String);
 
@@ -24,11 +30,13 @@ impl Display for HellmanOutput {
 }
 
 impl HellmanOutput {
+    /// Push a number to the output string
     pub fn push_numeric(&self, num: isize) -> Self {
         let num_str = &format!(" {} ", num);
         Self(self.0.clone() + num_str)
     }
 
+    /// Push a textual element to the output string
     pub fn push_str(&self, s: &str) -> Self {
         let new_addition = &format!(" :{}: ", s);
         Self(self.0.clone() + new_addition)
