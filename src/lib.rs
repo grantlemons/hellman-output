dc1d5fe12f3bfc65a3b69fecef0d5891033ebeb6
Grant Lemons <grantlemons@aol.com>
2025-02-19 11:23:11 -0700

    rework how OUTPUT is added on the front

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,8 @@ use std::{fmt::Display, str::FromStr, string::ParseError};
 /// - All numerical values required by an assignment must have whitespace (which includes end of line) around them.
 /// - Required textual output should be surrounded by single colons (:).
 /// - The order for results is assignment specific and important, they must emanate from the application in the order dictated by the assignment.
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Default, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct HellmanOutput(String);
-
-impl Default for HellmanOutput {
-    fn default() -> Self {
-        Self("OUTPUT".to_string())
-    }
-}
 
 impl FromStr for HellmanOutput {
     type Err = ParseError;
@@ -25,20 +19,20 @@ impl FromStr for HellmanOutput {
 
 impl Display for HellmanOutput {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+        write!(f, "OUTPUT {}", self.0.trim_end())
     }
 }
 
 impl HellmanOutput {
     /// Push a number to the output string
     pub fn push_numeric(&self, num: isize) -> Self {
-        let num_str = &format!(" {} ", num);
+        let num_str = &format!("{} ", num);
         Self(self.0.clone() + num_str)
     }
 
     /// Push a textual element to the output string
     pub fn push_str(&self, s: &str) -> Self {
-        let new_addition = &format!(" :{}: ", s);
+        let new_addition = &format!(":{}: ", s);
         Self(self.0.clone() + new_addition)
     }
 }
